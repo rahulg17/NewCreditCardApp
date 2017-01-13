e56bc48e8c52e241c26f8219d29c85b0816777f6
rahulg17 <rahulgd77@yahoo.co.in>
2017-01-13 11:43:24 +0530

    changes in profile pojo

--- src/main/java/com/devops/Profile.java
+++ src/main/java/com/devops/Profile.java
@@ -11,6 +11,144 @@ public class Profile {
 
     private String creditCard;
 
+    private String titleCust;
+
+    private String dob;
+
+    private String passNumber;
+
+    private String nationality;
+
+    private String addLine;
+
+    private String country;
+
+    private String occupation;
+
+    private String anualIncome;
+
+
+    /**
+     * @return the titleCust
+     */
+    public String getTitleCust() {
+        return this.titleCust;
+    }
+
+    /**
+     * @param titleCust
+     *            the titleCust to set
+     */
+    public void setTitleCust(final String titleCust) {
+        this.titleCust = titleCust;
+    }
+
+    /**
+     * @return the dob
+     */
+    public String getDob() {
+        return this.dob;
+    }
+
+    /**
+     * @param dob
+     *            the dob to set
+     */
+    public void setDob(final String dob) {
+        this.dob = dob;
+    }
+
+    /**
+     * @return the passNumber
+     */
+    public String getPassNumber() {
+        return this.passNumber;
+    }
+
+    /**
+     * @param passNumber
+     *            the passNumber to set
+     */
+    public void setPassNumber(final String passNumber) {
+        this.passNumber = passNumber;
+    }
+
+    /**
+     * @return the nationality
+     */
+    public String getNationality() {
+        return this.nationality;
+    }
+
+    /**
+     * @param nationality
+     *            the nationality to set
+     */
+    public void setNationality(final String nationality) {
+        this.nationality = nationality;
+    }
+
+    /**
+     * @return the addLine
+     */
+    public String getAddLine() {
+        return this.addLine;
+    }
+
+    /**
+     * @param addLine
+     *            the addLine to set
+     */
+    public void setAddLine(final String addLine) {
+        this.addLine = addLine;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return this.country;
+    }
+
+    /**
+     * @param country
+     *            the country to set
+     */
+    public void setCountry(final String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the occupation
+     */
+    public String getOccupation() {
+        return this.occupation;
+    }
+
+    /**
+     * @param occupation
+     *            the occupation to set
+     */
+    public void setOccupation(final String occupation) {
+        this.occupation = occupation;
+    }
+
+    /**
+     * @return the anualIncome
+     */
+    public String getAnualIncome() {
+        return this.anualIncome;
+    }
+
+    /**
+     * @param anualIncome
+     *            the anualIncome to set
+     */
+    public void setAnualIncome(final String anualIncome) {
+        this.anualIncome = anualIncome;
+    }
+
+
     /**
      * Gets the name.
      *

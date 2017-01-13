f23b3ac5c7a2fd267e268fa57b2244055d988fab
rahulg17 <rahulgd77@yahoo.co.in>
2017-01-13 11:44:29 +0530

    changes in controller

--- src/main/java/com/devops/ProfileServletController.java
+++ src/main/java/com/devops/ProfileServletController.java
@@ -1,14 +1,8 @@
 package com.devops;
 
-/**
- * <p>
- * <b> TODO : Insert description of the class's responsibility/role. </b>
- * </p>
- */
+import java.sql.Connection;
+import java.sql.Statement;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,16 +13,63 @@ public class ProfileServletController extends HttpServlet {
 
     public void doPost(final HttpServletRequest request, final HttpServletResponse response) {
         System.out.println("----- Registration -----");
+        Connection conn = null;
+        Statement stmt = null;
+
         try {
 
             String name = request.getParameter("name");
             String number = request.getParameter("number");
             String city = request.getParameter("city");
+            String dob = request.getParameter("dob");
+            String passNumber = request.getParameter("passNumber");
+            String nationality = request.getParameter("nationality");
+            String addLine = request.getParameter("addLine");
+            String country = request.getParameter("country");
+            String titleCust = request.getParameter("titleCust");
+
+            String occupation = request.getParameter("occupation");
+            String anualIncome = request.getParameter("anualIncome");
+
 
             String creditCard = request.getParameter("creditCard");
             System.out.println("**** creditCard = " + creditCard);
 
+
             Profile profile = new Profile();
+            profile.setName(name);
+            profile.setNumber(number);
+            profile.setCity(city);
+            profile.setCreditCard(creditCard);
+            profile.setAddLine(addLine);
+            profile.setAnualIncome(anualIncome);
+            profile.setCountry(country);
+            profile.setDob(dob);
+            profile.setPassNumber(passNumber);
+            profile.setTitleCust(titleCust);
+            profile.setOccupation(occupation);
+            profile.setNationality(nationality);
+
+
+            request.setAttribute("cust", profile);
+            request.getRequestDispatcher("/response.jsp").forward(request, response);
+
+
+            /*
+             * conn = createDatabaseConnection(); stmt =
+             * conn.createStatement();
+             *
+             * PreparedStatement ps = conn.prepareStatement(
+             * "insert into TEST.CUSTOMER values(?,?,?,?)");
+             *
+             * ps.setString(1, name); ps.setString(2, number); ps.setString(3,
+             * city); ps.setString(4, creditCard);
+             *
+             * int i = ps.executeUpdate(); if (i > 0) { System.out.println(
+             * "You are successfully registered..."); }
+             */
+
+
             profile.setName(name);
             profile.setNumber(number);
             profile.setCity(city);
@@ -37,12 +78,23 @@ public class ProfileServletController extends HttpServlet {
             request.setAttribute("cust", profile);
             request.getRequestDispatcher("/response.jsp").forward(request, response);
 
-        } catch (ServletException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+/*
+ * public static Connection createDatabaseConnection() throws SQLException,
+ * ClassNotFoundException {
+ *
+ * String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+ * Class.forName(driver); String url =
+ * "jdbc:derby:/SWDTOOLS/ECLIPSE-4.5.1/jdk1.8.0_66/db/bin/testDB";
+ *
+ *
+ * Connection c = DriverManager.getConnection(url); return c;
+ *
+ * } }
+ */
+
